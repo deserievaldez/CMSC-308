fe8498ad8e51365713cc31a6bfd51426d50ff170
deserievaldez <107237092+deserievaldez@users.noreply.github.com>
2022-09-14 22:23:49 +0800

    Add files via upload

---
+++ Activity/Activity 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Activity_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Email = {"Deserie@gmail.com",
+                              "Valdez@gmail.com" ,
+                              "Placa@gmail.com" };
+            string[] Username = {"Deserie",
+                                 "Valdez" ,
+                                 "Placa"};
+            string[] Password = {"Deserie0",
+                                 "Valdez1",
+                                 "Placa2"};
+
+            Console.Write("Index : ");
+
+            int Index = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Email : " + Email [Index]);
+            Console.WriteLine("Username : " + Username [Index]);
+            Console.WriteLine("Password : " + Password [Index]);
+
+
+        }
+    }
+}
