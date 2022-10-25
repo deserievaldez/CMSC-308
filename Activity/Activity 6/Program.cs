2177c2dceef92137cf7f2506186f0112b12a5423
deserievaldez <107237092+deserievaldez@users.noreply.github.com>
2022-10-25 21:46:05 +0800

    Add files via upload

---
+++ Activity/Activity 6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Activity6
+{
+	class program
+	{
+		public static void Main(string[] args)
+		{
+			string data = "SXCOMPUTER";
+			List<char> code = new List<char>();
+			code.AddRange(data);
+            int result;
+            Console.WriteLine("X C O M P U T E R S");
+			Console.Write("Enter Code: ");
+			string input = Console.ReadLine();
+			char[] charArr = input.ToCharArray();
+            Console.Write("Equal Value: ");
+            foreach (char item in charArr)
+			{
+                result = code.IndexOf(item) ;
+				Console.Write("{0}", result);
+			}
+		}
+	}
+}
